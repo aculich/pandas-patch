4fce71cf9d71619195d87f736ba21d56632744ca
Eric Fourrier <ericfourrier0@gmail.com>
2015-02-24 11:00:15 -0800

    Speed up test by removing Setup that was runned for each tests

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ Created on Wed Feb 11 10:33:16 2015
 
 Purpose : Automated test suites with unitest
 run "python -m unittest -v test" in the module directory to run the tests 
+
+This is not a clean coding but it is way faster than using a setup function
 """
 
 #########################################################
@@ -16,83 +18,77 @@ import unittest2 as unittest
 from pandas_patch import *
 import numpy as np 
 
-
+test_df = pd.read_csv('lc_test.csv')
+test_df['na_col'] = np.nan
+test_df['constant_col'] = 'constant'
+test_df['duplicated_column'] = test_df.id
+test_df['many_missing_70'] = [1]*300 + [np.nan] * 700
+test_df['num_var'] = range(test_df.shape[0])
 flatten_list = lambda x: [y for l in x for y in flatten_list(l)] if isinstance(x,list) else [x]
+
+#flatten_list = lambda x: [y for l in x for y in flatten_list(l)] if isinstance(x,list) else [x]
 #########################################################
 # Writing the tests  
 #########################################################
 
 class TestPandasPatch(unittest.TestCase):
 
-    def setUp(self):
-        """ Creating test datasets """
-        test_df = pd.read_csv('lc_test.csv')
-        test_df['na_col'] = np.nan
-        test_df['constant_col'] = 'constant'
-        test_df['duplicated_column'] = test_df.id
-        test_df['many_missing_70'] = [1]*300 + [np.nan] * 700
-        test_df['num_var'] = range(test_df.shape[0])
-        self.df_one = test_df
-        self.nacolcount = self.df_one.nacolcount()
-        self.narowcount = self.df_one.narowcount()
-        self.manymissing = self.df_one.manymissing(0.7)
-        self.constantcol = self.df_one.constantcol()
-        self.dfnum = self.df_one.dfnum()
-        self.detectkey = self.df_one.detectkey()
-        self.findupcol = self.df_one.findupcol()
-        self.clean_df = self.df_one.clean_df().columns
-        
     def test_sample_df(self):
-        self.assertEqual(len(self.df_one.sample_df(pct = 0.061)),
-                         0.061 * float(self.df_one.shape[0]))
+        self.assertEqual(len(test_df.sample_df(pct = 0.061)),
+                         0.061 * float(test_df.shape[0]))
 
     def test_nrow(self):
-        self.assertEqual(self.df_one.nrow(),self.df_one.shape[0])
+        self.assertEqual(test_df.nrow(),test_df.shape[0])
     
     def test_col(self):
-        self.assertEqual(self.df_one.ncol(),self.df_one.shape[1])
+        self.assertEqual(test_df.ncol(),test_df.shape[1])
         
     def test_nacolcount_capture_na(self):
-        self.assertEqual(self.nacolcount.loc['na_col','Napercentage'],1.0)
-        self.assertEqual(self.nacolcount.loc['many_missing_70','Napercentage'],0.7)
+        nacolcount = test_df.nacolcount()
+        self.assertEqual(nacolcount.loc['na_col','Napercentage'],1.0)
+        self.assertEqual(nacolcount.loc['many_missing_70','Napercentage'],0.7)
         
     def test_nacolcount_is_type_dataframe(self):
-        self.assertIsInstance(self.nacolcount,pd.core.frame.DataFrame)
+        self.assertIsInstance(test_df.nacolcount(),pd.core.frame.DataFrame)
     
     def test_narowcount_capture_na(self):
-        self.assertEqual(sum(self.narowcount['Nanumber'] > 0),self.df_one.nrow())
+        narowcount = test_df.narowcount()
+        self.assertEqual(sum(narowcount['Nanumber'] > 0),test_df.nrow())
     
     def test_narowcount_is_type_dataframe(self):
-        self.assertIsInstance(self.narowcount,pd.core.frame.DataFrame)
+        narowcount = test_df.narowcount()
+        self.assertIsInstance(narowcount,pd.core.frame.DataFrame)
     
     def test_manymissing_capture(self):
-        self.assertIn('many_missing_70',self.manymissing)
-        self.assertIn('na_col',self.manymissing)
+        manymissing = test_df.manymissing(0.7)
+        self.assertIn('many_missing_70',manymissing)
+        self.assertIn('na_col',manymissing)
     
     def test_constant_col_capture(self):
-        self.assertIn('constant_col',self.constantcol)
+        constantcol = test_df.constantcol()
+        self.assertIn('constant_col',constantcol)
     
     def test_dfnum_check_col(self):
-        self.assertNotIn('constant_col', self.dfnum)
-        self.assertIn('num_var', self.dfnum)
-        self.assertIn('many_missing_70', self.dfnum)
+        dfnum = test_df.dfnum()
+        self.assertNotIn('constant_col', dfnum)
+        self.assertIn('num_var', dfnum)
+        self.assertIn('many_missing_70',dfnum)
         
     def test_detectkey_check_col(self):
-        self.assertIn('id', self.detectkey)
+        detectkey = test_df.detectkey()
+        self.assertIn('id', detectkey)
     
     def test_findupcol_check(self):
-        self.assertIn(['loan_amnt', 'funded_amnt'],self.findupcol)
-        self.assertIn(['id', 'duplicated_column'],self.findupcol)
-        self.assertNotIn('member_id',flatten_list(self.findupcol))
+        findupcol = test_df.findupcol()
+        self.assertIn(['loan_amnt', 'funded_amnt'],findupcol)
+        self.assertIn(['id', 'duplicated_column'],findupcol)
+        self.assertNotIn('member_id',flatten_list(findupcol))
         
     def test_clean_df(self):
-        self.assertTrue(all([e not in self.clean_df for e in ['constant_col',
+        clean_df = test_df.clean_df().columns
+        self.assertTrue(all([e not in clean_df for e in ['constant_col',
                             'na_col','duplicated_column']]))
-        self.assertIn('id',self.clean_df)
-
-    def tearDown(self):
-        """ Cleaning the environnement """
-        self.df_one = None
+        self.assertIn('id',clean_df)
 
 
 # Adding new tests sets 
